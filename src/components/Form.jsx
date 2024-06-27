86bfc92e83ff0d2db4ccc18cd73147e766bbca88
carleoj <jimroep@gmail.com>
2024-06-27 22:11:24 +0800

    "added alert after submit:

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import emailjs from 'emailjs-com';
 
-
 const Form = () => {
   useEffect(() => {
     emailjs.init('0Vg9DbUbUL2GdA73Q'); // Replace with your user ID from EmailJS
@@ -11,35 +10,36 @@ const Form = () => {
     event.preventDefault();
     emailjs.sendForm('contact_service', 'contact_form', event.target)
       .then((response) => {
-        console.log('SUCCESS!', response.status, response.text);
-        // Optionally, you can handle success behavior like showing a success message
+        alert('Email sent successfully!');
+        event.target.reset(); // Reset the form after sending
       }, (error) => {
-        console.log('FAILED...', error);
-        // Optionally, you can handle error behavior like showing an error message
+        alert('Failed to send email. Please try again later.');
+        console.error('Email send error:', error);
       });
-    event.target.reset(); // Reset the form after sending
   };
 
   const style = {
     marginBottom: "130px",
     height: "200px",
     display: "grid"
-  }
+  };
+
   const boxStyle = {
     height: "100px",
     padding: "30px",
     margin: "10px",
     borderRadius: "5px"
-  }
+  };
+
   const inp = {
     height: "30px",
     borderRadius: "5px"
-  }
+  };
 
   return (
     <div>
       <form style={style} id="contact-form" onSubmit={handleSubmit}>
-      <h2>Send me an Email</h2>
+        <h2>Send me an Email</h2>
         <input type="hidden" name="contact_number" value="697483" />
         <label>Name</label>
         <input style={inp} type="text" name="user_name" />
@@ -50,7 +50,7 @@ const Form = () => {
         
         <input className='btn2' type="submit" value="Send" />
       </form>
-      <br></br><br></br><br></br><br></br>
+      <br /><br /><br /><br />
     </div>
   );
 };
